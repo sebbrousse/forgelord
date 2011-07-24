59f281ec2742f4354154d01cead89aaf5e56537a
Sébastien BROUSSE <seb.brousse@gmail.com>
2011-07-24 23:13:12 +0200

    correct the HomePageTest du to HomePage refactoring

--- forgelord/src/test/java/com/forgelord/wicket/home/HomePageTest.java
+++ forgelord/src/test/java/com/forgelord/wicket/home/HomePageTest.java
@@ -3,6 +3,7 @@ package com.forgelord.wicket.home;
 import org.junit.Test;
 
 import com.forgelord.test.wicket.AbstractWicket;
+import com.forgelord.wicket.pages.home.HomePage;
 
 public class HomePageTest extends AbstractWicket {
 
