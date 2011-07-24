e43b69875774994d28688b9d766dd81727b12f65
Sébastien BROUSSE <seb.brousse@gmail.com>
2011-07-24 23:23:09 +0200

    move a typo

---
+++ forgelord/src/test/java/com/forgelord/wicket/pages/home/HomePageTest.java
@@ -0,0 +1,20 @@
+package com.forgelord.wicket.pages.home;
+
+import org.junit.Test;
+
+import com.forgelord.test.wicket.AbstractWicket;
+import com.forgelord.wicket.pages.home.HomePage;
+
+public class HomePageTest extends AbstractWicket {
+
+	@Test
+	public void shouldHaveHomeForTitle() {
+		tester.startPage(HomePage.class);
+		tester.assertRenderedPage(HomePage.class);
+	}
+
+
+}
+
+
+
