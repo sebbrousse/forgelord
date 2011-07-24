4c2a916b20a326207af58d1c81eef5618f9233f7
Sébastien BROUSSE <seb.brousse@gmail.com>
2011-07-24 23:01:19 +0200

    add a model of user

---
+++ forgelord/src/main/java/com/forgelord/model/User.java
@@ -0,0 +1,21 @@
+package com.forgelord.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User implements Serializable {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String login;
+	private String fullName;
+	private String password;
+	
+	
+}
