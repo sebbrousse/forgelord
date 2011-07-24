16df3f7fdd27970365e3d7a42a9dbe6459e9a848
Sébastien BROUSSE <seb.brousse@gmail.com>
2011-07-24 23:29:23 +0200

    add a mock for UserRepo in ListUsersPageTest

--- forgelord/src/test/java/com/forgelord/wicket/pages/users/ListUsersTest.java
+++ forgelord/src/test/java/com/forgelord/wicket/pages/users/ListUsersTest.java
@@ -1,11 +1,22 @@
 package com.forgelord.wicket.pages.users;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
 
+import com.forgelord.repositories.UserRepository;
 import com.forgelord.test.wicket.AbstractWicket;
 
 public class ListUsersTest extends AbstractWicket {
 
+	@Mock
+	private UserRepository userRepo;
+	
+	@Before  
+	public void setup() {
+		ctx.putBean(userRepo);
+	}  
+	
 	@Test
 	public void shouldHaveATitle() {
 		tester.startPage(ListUsersPage.class);
